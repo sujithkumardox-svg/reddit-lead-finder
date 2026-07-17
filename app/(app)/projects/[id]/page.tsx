318f1147c1c814a801bbbfa765a1386c04aa06b1
Sujith <sujithkumardox@gmail.com>
2026-07-17 16:17:11 +0530

    Prevent save button from overlapping dashboard content

--- app/(app)/projects/[id]/page.tsx
+++ app/(app)/projects/[id]/page.tsx
@@ -26,7 +26,7 @@ export default async function ProjectPage({
   }
 
   return (
-    <main className="flex flex-1 flex-col gap-6 px-6 py-8">
+    <main className="flex flex-1 flex-col gap-6 px-6 pt-8 pb-24">
       <div>
         <h1 className="text-2xl font-semibold tracking-tight">{project.name}</h1>
         <a
